0febe7162bb60f27eb611b5ae47ab12967fee58f
ldi0122 <ldi0122@naver.com>
2022-09-13 22:58:11 +0900

    #1436, #7568

---
+++ BruteForce.py
@@ -0,0 +1,44 @@
+#덩치 7568번
+
+# num = int(input())
+# m=[]
+# n=[]
+
+# result=[]
+# for i in range(num):
+#     x,y=map(int,input().split())
+#     m.append(x)
+#     n.append(y)
+
+# for j in range(num):
+#     cnt=1
+#     for k in range(num):
+#         if m[j]<m[k] and n[j]<n[k]:
+#             cnt+=1
+#     result.append(cnt)
+# print(result)
+    
+#체스판 다시 칠하기 1018번
+
+n,m =map(int,input().split())
+chess=[input()for 1 in range(n)]
+print(chess)
+
+#영화 감독숌 1436번
+
+# n = int(input())
+# count=0
+# num=665
+
+
+# while True:
+#     if "666" in str(num):
+#         count+=1
+#         if count==n:
+#             print(num)
+#             break
+#         num+=1
+#     else:
+#         num+=1
+
+
